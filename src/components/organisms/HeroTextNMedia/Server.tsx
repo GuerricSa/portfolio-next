db2512dbffe743102e2abeb1f317b327e0db9ae6
GuerricSa <guerric.sant@gmail.com>
2025-06-09 10:27:55 +0200

    HeroTextNMedia calendly button test modification

--- src/components/organisms/HeroTextNMedia/Server.tsx
+++ src/components/organisms/HeroTextNMedia/Server.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import HeroTextNMediaClient from './Client';
 
 export default function HeroTextNMedia(): React.ReactElement {
-  return <HeroTextNMediaClient calendlyUrl="https://calendly.com/guerric-sant" calendlyText="Discutons de votre projet" calendlyStyle="quaternary" />
+  return <HeroTextNMediaClient calendlyUrl="https://calendly.com/guerric-sant" calendlyText="On s'appelle ?" calendlyStyle="quaternary" />
 
 }
